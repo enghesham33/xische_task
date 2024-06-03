2e45d458ec23b23046486123f40e06942ec94c8e
Hesham Khaled <heshamkhaled92@gmail.com>
2024-06-03 18:10:26 +0300

    Initial commit

---
+++ Features/Listing-Screen/Listing-Screen/Listing_Screen.h
@@ -0,0 +1,18 @@
+//
+//  Listing_Screen.h
+//  Listing-Screen
+//
+//  Created by Hesham Khaled on 03/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Listing_Screen.
+FOUNDATION_EXPORT double Listing_ScreenVersionNumber;
+
+//! Project version string for Listing_Screen.
+FOUNDATION_EXPORT const unsigned char Listing_ScreenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Listing_Screen/PublicHeader.h>
+
+
---
+++ Xishe_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xishe_task
+//
+//  Created by Hesham Khaled on 03/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
